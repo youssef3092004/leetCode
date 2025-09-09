427047eadbc8fd14fdd581f86a44f10cd37a8316
joe <joe404prodev@gmail.com>
2025-09-09 22:42:52 +0300

    fix: correct singleNumber implementation
    - Initialize frequency map with `let freq = {}`
    - Replace invalid `for (key and value in freq)` with proper `for...in` loop
    - Fix condition to check for `value === 1` instead of `0`
    - Ensure returned key is converted back to a number

---
+++ easy/136. Single Number136. Single Number.js
@@ -0,0 +1,17 @@
+/**
+ *  * @param {number[]} nums
+ *   * @return {number}
+ *    */
+var singleNumber = function(nums) {
+	    let freq = {};
+	    for (let i = 0; i < nums.length; i++) {
+		            if (nums[i] in freq)
+			                freq[nums[i]]++;
+		            else
+			                freq[nums[i]] = 1;
+		        }
+	    for (let key in freq) {
+		            if (freq[key] === 1)
+			            return Number(key);
+		        }
+};
